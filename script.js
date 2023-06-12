a31dd3e6695a6fe60d0abc6d3550305c15f8dfcf
Meni <menisadi@users.noreply.github.com>
2023-06-12 13:21:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function calculateRevenue() {
   var initialAmount = parseFloat(document.getElementById("initialAmount").value);
   var initialYear = parseInt(document.getElementById("initialYear").value);
-  var interestRate = parseFloat(document.getElementById("interestRate").value);
-  var initialCPI = parseFloat(document.getElementById("initialCPI").value);
-  var currentCPI = parseFloat(document.getElementById("currentCPI").value);
+  var interestRate = parseFloat(document.getElementById("interestRate").value) / 100;
+  var initialCPI = parseFloat(document.getElementById("initialCPI").value) / 100;
+  var currentCPI = parseFloat(document.getElementById("currentCPI").value) / 100;
   const currentYear = new Date().getFullYear(); // Get the current year
   const timePeriod = currentYear - initialYear; // Calculate the time period
   
