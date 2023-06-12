0095833906d1343449a6927855cfa8b6bc1267c6
Meni <menisadi@users.noreply.github.com>
2023-06-12 13:06:17 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function calculateRevenue() {
+  var initialAmount = parseFloat(document.getElementById("initialAmount").value);
+  var initialYear = parseInt(document.getElementById("initialYear").value);
+  var interestRate = parseFloat(document.getElementById("interestRate").value);
+  var initialCPI = parseFloat(document.getElementById("initialCPI").value);
+  var currentCPI = parseFloat(document.getElementById("currentCPI").value);
+  
+  // Calculate the revenue
+  var revenue = initialAmount * ((currentCPI / initialCPI) - 1);
+  
+  // Display the revenue result
+  var resultElement = document.getElementById("revenueResult");
+  resultElement.textContent = revenue.toFixed(2) + " NIS";
+}
